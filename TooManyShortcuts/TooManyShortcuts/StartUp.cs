f652c01aa73af33fb75e227efc7b170407b94aa6
LouisMehring <Louis.Mehring@stadt-frankfurt.de>
2015-11-27 11:15:50 +0100

    Add StartUp Path

---
+++ TooManyShortcuts/TooManyShortcuts/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace TooManyShortcuts
+{
+    class StartUp
+    {
+     public void StartUPScheiße {
+            "C:\Users\Mehring\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+     }
+        
+     
+    }
+}
