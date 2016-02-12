891309891650940a1ddddd2729935473a5aac2a2
Driven1 <maxrill@gmx.de>
2016-02-12 11:09:50 +0100

    xml stuff

---
+++ TooManyShortcuts/TooManyShortcuts/Shortcut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooManyShortcuts
+{
+    [Serializable()]
+    public class Shortcut
+    {
+        private string shorthand;
+
+        public string Shorthand
+        {
+            get { return shorthand; }
+            set { shorthand = value; }
+        }
+
+        private string keycombo;
+
+        public string Keycombo
+        {
+            get { return keycombo; }
+            set { keycombo = value; }
+        }
+
+        private string path;
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+    }
+}
---
+++ TooManyShortcuts/TooManyShortcuts/ShortcutList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TooManyShortcuts
+{
+    [XmlRoot]
+    class ShortcutList
+    {
+        public List<Shortcut> Shortcuts { get; set; }
+    }
+}
