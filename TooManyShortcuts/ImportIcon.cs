2c80ad5814483871cb5e511a4e4bc0ce0deb1c2a
LouisMehring <Louis.Mehring@web.de>
2016-02-19 10:34:03 +0100

    Allgemeine Verbesserungen // Neue Klassen etc.

---
+++ TooManyShortcuts/ImportIcon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Drawing;
+
+
+
+namespace TooManyShortcuts
+{
+   public  class ImportIcon
+    {
+        public Icon ExtractAssociatedIconEx(string path) {
+          Icon ico = Icon.ExtractAssociatedIcon(path);
+           return ico; 
+        }
+  
+    }
+}
--- TooManyShortcuts/Program.cs
+++ TooManyShortcuts/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TooManyShortcuts
@@ -16,7 +15,7 @@ namespace TooManyShortcuts
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
