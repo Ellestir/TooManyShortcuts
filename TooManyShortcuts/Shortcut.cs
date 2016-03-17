1e8d1a28242547da2201da0b50e4f974ca683873
Max Rill <maxrill@gmx.de>
2016-03-17 16:47:12 +0100

    XML-Serialisierung

---
+++ TooManyShortcuts/Shortcut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TooManyShortcuts
+{
+    //Grundgerüst, Verweis auf die Icons fehlt noch, evtl. noch Name oder ID
+    [Serializable()]
+    public class Shortcut
+    {
+        private string shorthand;
+
+        public string Shorthand
+        {
+            get { return shorthand; }
+            set { shorthand = value; }
+        }
+
+        private string keycombo;
+
+        public string Keycombo
+        {
+            get { return keycombo; }
+            set { keycombo = value; }
+        }
+
+        private string path;
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+    }
+}
