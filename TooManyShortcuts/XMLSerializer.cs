42c48f2e40475b4d4787dedc4cdba175792ef93e
Driven1 <maxrill@gmx.de>
2016-05-11 13:59:43 +0200

    Namensänderungen innerhalb der Serializer-Funktion (nur interne Objekte)

--- TooManyShortcuts/XMLSerializer.cs
+++ TooManyShortcuts/XMLSerializer.cs
@@ -17,27 +17,27 @@ namespace TooManyShortcuts
         public static void Serialize(XMLShortcutList list, string xmlpath)
         {
             // Erstellt eine neue Instanz der XmlSerializer-Klasse für den Typ unserer Listenklasse
-            XmlSerializer SerializerObj = new XmlSerializer(typeof(XMLShortcutList));
+            XmlSerializer serializerObj = new XmlSerializer(typeof(XMLShortcutList));
 
-            StringWriter ValidationStream = new StringWriter();
+            StringWriter validationStream = new StringWriter();
 
-            SerializerObj.Serialize(ValidationStream, list);
-            ValidationStream.Close();
-            string ValidationData = ValidationStream.ToString();
+            serializerObj.Serialize(validationStream, list);
+            validationStream.Close();
+            string validationData = validationStream.ToString();
 
             //validiert das bei der Serialisierung entstandene XML gegen das Schema bevor es gespeichert wird
-            string ValidationResult = ValidateXML(ValidationData, list.SchemaLocation);
+            string validationResult = ValidateXML(validationData, list.SchemaLocation);
 
-            if (ValidationResult == "ok")
+            if (validationResult == "ok")
             {
                 // Erstellt eine neue StreamWriter-Instanz um die Daten in eine Datei zu schreiben
-                TextWriter WriteFileStream = new StreamWriter(xmlpath);
+                TextWriter writeFileStream = new StreamWriter(xmlpath);
 
                 // Alle Shortcuts werden als einzelne Child-Elemente("Shortcut") von "Shortcuts" geschrieben
-                SerializerObj.Serialize(WriteFileStream, list);
+                serializerObj.Serialize(writeFileStream, list);
 
                 // Aufräumarbeiten
-                WriteFileStream.Close();
+                writeFileStream.Close();
             }
             //Platzhalter um irgendetwas mit der Exception-Message zu tun
         }
@@ -48,20 +48,20 @@ namespace TooManyShortcuts
             if (File.Exists(xmlpath))
             {
                 // Erstellt eine neue Instanz der XmlSerializer-Klasse für den Typ unserer Listenklasse
-                XmlSerializer SerializerObj = new XmlSerializer(typeof(XMLShortcutList));
+                XmlSerializer serializerObj = new XmlSerializer(typeof(XMLShortcutList));
 
                 // Erstellt einen neuen FileStream um die Daten zu lesen
 
-                FileStream ReadFileStream = new FileStream(xmlpath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                FileStream readFileStream = new FileStream(xmlpath, FileMode.Open, FileAccess.Read, FileShare.Read);
 
                 // Deserialisiert das XML-File und erzeugt daraus ein XMLShortcutList-Objekt
-                XMLShortcutList LoadedObj = (XMLShortcutList)SerializerObj.Deserialize(ReadFileStream);
+                XMLShortcutList loadedObj = (XMLShortcutList)serializerObj.Deserialize(readFileStream);
 
                 //Überträgt die Shortcut-Liste von dem deserialisierten Objekt in das übergebene XMLShortcutList-Objekt (aktualisiert also die Liste, die bereits verwendet wird)
-                list.Shortcuts = LoadedObj.Shortcuts;
+                list.Shortcuts = loadedObj.Shortcuts;
 
                 // Aufräumarbeiten
-                ReadFileStream.Close();
+                readFileStream.Close();
             }
         }
         //Prüft, ob das gegebene XML dem gegebenen Schema entspricht
